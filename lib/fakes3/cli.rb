d9bb49caf48e1c17f04565a2f3158db9b42242a4
JGW Maxwell <jgwmaxwell@gmail.com>
2015-02-15 16:22:05 +0000

    hostname to env

--- lib/fakes3/cli.rb
+++ lib/fakes3/cli.rb
@@ -9,7 +9,7 @@ module FakeS3
     desc "server", "Run a server on a particular hostname"
     method_option :root, :type => :string, :aliases => '-r', :required => true
     method_option :address, :type => :string, :aliases => '-a', :required => false, :desc => "Bind to this address. Defaults to 0.0.0.0"
-    method_option :hostname, :type => :string, :aliases => '-H', :desc => "The root name of the host.  Defaults to s3.amazonaws.com."
+    method_option :hostname, :type => :string, :aliases => '-H', :default => ENV['HOSTNAME'], :desc => "The root name of the host.  Defaults to s3.amazonaws.com."
     method_option :limit, :aliases => '-l', :type => :string, :desc => 'Rate limit for serving (ie. 50K, 1.0M)'
     method_option :sslcert, :type => :string, :desc => 'Path to SSL certificate'
     method_option :sslkey, :type => :string, :desc => 'Path to SSL certificate key'
